58f87a47d9885b38f9838dd72401a97d3dd3bce2
Joao Vitor <joaovitorcfdev@gmail.com>
2022-06-26 11:48:14 -0300

    variaveis

---
+++ variaveis.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nome = 'João';
+  print(nome);
+
+  int valorVariavel = 10;
+  print(valorVariavel);
+
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+
+  List<String> listaDePalavras = ['João', 'Vitor'];
+  print('${listaDePalavras[0]} ${listaDePalavras[1]}');
+}
