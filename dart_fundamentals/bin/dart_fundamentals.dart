992d5a24309d6820c7d28e5b4bb0eff9d08258a1
Joao Vitor <joaovitorcfdev@gmail.com>
2022-10-17 22:28:44 -0300

    variables

--- dart_fundamentals/bin/dart_fundamentals.dart
+++ dart_fundamentals/bin/dart_fundamentals.dart
@@ -1,5 +1,3 @@
-import 'package:dart_fundamentals/dart_fundamentals.dart' as dart_fundamentals;
-
 void main(List<String> arguments) {
-  print('Hello world: ${dart_fundamentals.calculate()}!');
+  print('Hello world');
 }
---
+++ dart_fundamentals/lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  // camelCase
+  num qualquerNumero = 1;
+  int idade = 25;
+  double valorIphone = 12.000;
+  String nomeCurso = 'Academia do Flutter';
+
+  Object objetoQualquer = 10;
+  dynamic dynamicQualquer = 'Qualquer coisa';
+}
