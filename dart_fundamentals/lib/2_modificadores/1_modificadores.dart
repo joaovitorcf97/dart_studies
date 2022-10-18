a9b5c4e29aed0ff5daef2a03b1b834cff3c457aa
Joao Vitor <joaovitorcfdev@gmail.com>
2022-10-17 22:59:35 -0300

    modifiers final and const

---
+++ dart_fundamentals/lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,12 @@
+void main() {
+  String nomeCompleto = 'João';
+  print(nomeCompleto);
+  nomeCompleto = 'Vitor';
+  print(nomeCompleto);
+
+  // final (Utilize sem moderação)
+  final nomeCompletoFinal = 'João';
+
+  // const
+  const nomeCompletoConst = 'João';
+}
